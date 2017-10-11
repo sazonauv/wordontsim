8b68b2d4405e286121f7b82a0d61a0ca170dc00b
zhaohaoruo <1309273510@qq.com>
2017-10-11 13:46:07 +0100

    Update BioportalSimilarityExperiment.java
    remove coverData because we need it in another file. So we extract it as a module.

--- src/main/java/uk/ac/man/cs/exp/BioportalSimilarityExperiment.java
+++ src/main/java/uk/ac/man/cs/exp/BioportalSimilarityExperiment.java
@@ -1,26 +1,27 @@
 package uk.ac.man.cs.exp;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 
-import com.opencsv.CSVReader;
+import org.semanticweb.owlapi.model.OWLOntology;
+
 import com.opencsv.CSVWriter;
-import uk.ac.man.cs.io.Out;
+
+import uk.ac.man.cs.io.CoverageData;
 import uk.ac.man.cs.io.TermDataLoader;
 import uk.ac.man.cs.io.TermPair;
 import uk.ac.man.cs.ont.ClassFinder;
 import uk.ac.man.cs.ont.OntologyLoader;
 import uk.ac.man.cs.sim.SimilarityCalculator;
-
+//import wordontsim.RemoveOntologyRule;
+import uk.ac.man.cs.sim.RemoveOntologyRule;
 
 public class BioportalSimilarityExperiment {
 	
@@ -38,74 +39,92 @@ public class BioportalSimilarityExperiment {
 		TermDataLoader tdLoader = new TermDataLoader(analogyFile, relatednessFile, similarityFile);
 		BioportalSimilarityExperiment bioSimExp = new BioportalSimilarityExperiment(tdLoader.getAllPairs());
 		File coverageFile = new File(analogyFile.getParentFile(), "bioportal_coverage_distr.csv");
-		bioSimExp.calculateSimilarityScores(ontDir, coverageFile);
+		File reasonerTestFile = new File(analogyFile.getParentFile(), "ontology_reasoner_test.csv");
+		bioSimExp.calculateSimilarityScores(ontDir, coverageFile, reasonerTestFile);
 	}
 
 	public BioportalSimilarityExperiment(Set<TermPair> termPairs) {
 		this.termPairs = termPairs;
 	}
 	
-	private void calculateSimilarityScores(File ontDir, File coverageFile) throws IOException {
+	private void calculateSimilarityScores(File ontDir, File coverageFile, File reasonerTestFile) throws IOException {
 
-		Map<String, String> coverageData = getCoverageData(coverageFile);
-	    	
+		Map<String, String> coverageData = new CoverageData(coverageFile).getCoverageData();		
+		Map<String, String> reasonerData = new CoverageData(reasonerTestFile).getCoverageData();
 		File resultCSV = new File(coverageFile.getParent(), "bioportal_calculate_similarity.csv");
 	    	
 		List<String> row = new ArrayList<>();
 		Set<String> terms = new HashSet<>();
 		row.add("ontologies");
 		for(TermPair pair : termPairs){
-			String pairStr = pair.first +" && "+ pair.second;
-			row.add(pairStr);
+			row.add(pair.toString());
 			terms.add(pair.first);
 			terms.add(pair.second);
-		}  
-		Out.p(termPairs.size());
-		Out.p(terms.size());   	   	
+		}  			   	
 	    	
 		CSVWriter writer = new CSVWriter(new FileWriter(resultCSV));
 		writer.writeNext(row.toArray(new String[row.size()]));	    	
 	  
 		for (File ontFile : ontDir.listFiles()) {
-			Out.p(coverageData.get(ontFile.getName()));
-			List<String> scores = new ArrayList<>();
-			scores.add(ontFile.getName());
-	    		
-			log.info("checking "+ontFile.getName()+"\n");
-			OntologyLoader loader = new OntologyLoader(ontFile, true);
-			ClassFinder finder = new ClassFinder(loader.getOntology());
-			SimilarityCalculator sim = new SimilarityCalculator(finder);
-			sim.contains(terms);
-			if(coverageData.get(ontFile.getName()).equals("0")){
-				for(TermPair pair : termPairs){
-					scores.add("0.0"); 
-				}
+			
+			//List<String> scores = new ArrayList<>();
+			//scores.add(ontFile.getName());	    		
+			log.info("checking "+ontFile.getName()+ "with" +coverageData.get(ontFile.getName())+"terms\n");			
+			if(!coverageData.get(ontFile.getName()).equals("0")){				
+				if(!reasonerData.get(ontFile.getName()).equals("yes")
+						&&!reasonerData.get(ontFile.getName()).equals("big ontology with terms")						
+						&&!ontFile.getName().equals("galen.galen-ontology.1.owl.xml")						
+						&&!ontFile.getName().equals("ogdi.ontology-for-genetic-disease-investigations.3.owl.xml")
+						){
+					System.out.println(ontFile.getName());
+					List<String> scores = new ArrayList<>();
+					scores.add(ontFile.getName());	
+					OntologyLoader loader = new OntologyLoader(ontFile, true);
+					ClassFinder finder = new ClassFinder(loader.getOntology());
+					SimilarityCalculator sim = new SimilarityCalculator(finder);
+					sim.contains(terms);
+					for(TermPair pair : termPairs){
+							
+						String score = String.valueOf(sim.computeScore(pair.first, pair.second));
+						log.info(String.valueOf(sim.score(pair.first, pair.second))
+								+" "+pair.first+" "+pair.second+"\n");
+						scores.add(score);   					       
+					}					
+					writer.writeNext(scores.toArray(new String[scores.size()]));  
+				}				
 			}
-			else{
-				for(TermPair pair : termPairs){
-					String score = String.valueOf(sim.computeScore(pair.first, pair.second));
-					log.info(String.valueOf(sim.score(pair.first, pair.second))
-							+" "+pair.first+" "+pair.second+"\n");
-					scores.add(score);   					       
-				} 
-			}
-			writer.writeNext(scores.toArray(new String[scores.size()]));  		
+			
+			//test ontologies with reasoning problem
+			/*if(!coverageData.get(ontFile.getName()).equals("0")){
+				if(reasonerData.get(ontFile.getName()).equals("yes")
+					&&!ontFile.getName().equals("bof.biodiversity-ontology.2.owl.xml")
+					&&!ontFile.getName().equals("natpro.natural-products-ontology.1.owl.xml")
+					){
+				
+					System.out.println(ontFile.getName());
+					List<String> scores = new ArrayList<>();
+					scores.add(ontFile.getName());	
+					OntologyLoader loader = new OntologyLoader(ontFile, true);
+					OWLOntology ontology = loader.getOntology();
+					new RemoveOntologyRule(ontology).removeAll();;
+					ClassFinder finder = new ClassFinder(ontology);
+					SimilarityCalculator sim = new SimilarityCalculator(finder);
+					sim.contains(terms);
+					for(TermPair pair : termPairs){
+							
+						String score = String.valueOf(sim.computeScore(pair.first, pair.second));
+						log.info(String.valueOf(sim.score(pair.first, pair.second))
+								+" "+pair.first+" "+pair.second+"\n");
+						scores.add(score);   					       
+					}					
+					writer.writeNext(scores.toArray(new String[scores.size()]));  
+				}	
+			}*/	
+			
 		} 
 		writer.close();
 		log.info("Finish loop..");
-	}
-
-
-	private static Map<String, String> getCoverageData(File coverageFile) throws IOException {
-		Map<String, String> coverageData = new HashMap<>();
-		CSVReader reader = new CSVReader(new FileReader(coverageFile));
-		List<String[]> rows = reader.readAll();
-		rows.remove(rows.get(0));
-		for (String[] row : rows) {
-			coverageData.put(row[0], row[1]);
-		}
-		return coverageData;
-	}
-	
+	}	
 
 }
+
