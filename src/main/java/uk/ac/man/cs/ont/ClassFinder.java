12a8655bbe053509cb06bd867b72be5347c79e46
zhaohaoruo <1309273510@qq.com>
2017-10-19 18:07:46 +0100

    Update ClassFinder.java
    add a new construct function and new function init(Map<String,String> termWithLabel) for these file we want to add annotation

--- src/main/java/uk/ac/man/cs/ont/ClassFinder.java
+++ src/main/java/uk/ac/man/cs/ont/ClassFinder.java
@@ -1,11 +1,21 @@
 package uk.ac.man.cs.ont;
 
+import org.semanticweb.owlapi.model.AddAxiom;
+import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.parameters.Imports;
 
+import com.opencsv.CSVReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.semanticweb.owlapi.search.EntitySearcher.getAnnotationObjects;
@@ -16,23 +26,55 @@ import static org.semanticweb.owlapi.search.EntitySearcher.getAnnotationObjects;
 public class ClassFinder {
 
     private OWLOntology ontology;
-
+    private File file;
     private Map<String, OWLClass> nameClassMap;
 
     public ClassFinder(OWLOntology ontology) {
         this.ontology = ontology;
         init();
     }
+    
+    public ClassFinder(OWLOntology ontology, File file) throws IOException{
+    	 this.ontology = ontology;
+    	 this.file = file;
+         init(addAnnotation());
+    }
 
     private void init() {
-        nameClassMap = new HashMap<>();
-        for (OWLClass cl : ontology.getClassesInSignature(Imports.INCLUDED)) {
+        nameClassMap = new HashMap<String, OWLClass>();
+        for (OWLClass cl : ontology.getClassesInSignature(Imports.INCLUDED)) {       	
             nameClassMap.put(cl.getIRI().getShortForm().toLowerCase(), cl);
             Iterable<OWLAnnotation> annotations = getAnnotationObjects(cl, ontology);
             for (OWLAnnotation ann : annotations) {
                 nameClassMap.put(ann.getValue().toString().toLowerCase(), cl);
             }
         }
+    }
+    
+    private void init(Map<String,String> termWithLabel){
+    	nameClassMap = new HashMap<String, OWLClass>();
+        for (OWLClass cl : ontology.getClassesInSignature(Imports.INCLUDED)) { 
+        	String name = cl.getIRI().getShortForm().toLowerCase();
+			if(termWithLabel.keySet().contains(name)){
+				nameClassMap.put(termWithLabel.get(name), cl);
+			}
+            nameClassMap.put(name, cl);
+            Iterable<OWLAnnotation> annotations = getAnnotationObjects(cl, ontology);
+            for (OWLAnnotation ann : annotations) {
+                nameClassMap.put(ann.getValue().toString().toLowerCase(), cl);
+            }
+        }
+    }
+    
+    private Map<String,String> addAnnotation() throws IOException{
+    	CSVReader reader = new CSVReader(new FileReader(file));
+		List<String[]> rows = reader.readAll();
+		Map<String,String> termWithLabel = new HashMap();
+		for(String[] row:rows){
+			String name = IRI.create(row[0]).getShortForm().toLowerCase();
+			termWithLabel.put(name, row[1]);
+		}	
+		return termWithLabel;
     }
 
 
